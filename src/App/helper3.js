6ebe32cf2685c4a8846f601568288a0cf7b42dd3
Julie Dursema <jmdursema@gmail.com>
2017-12-09 10:40:17 -0700

    move api fetches into separate files

---
+++ src/App/helper3.js
@@ -0,0 +1,26 @@
+export const fetchPlanetInfo = async() => {
+  const fetchPlanets = await fetch(`https://swapi.co/api/planets/`);
+  const planetsData = await fetchPlanets.json();
+  const planetObjArray = await createPlanetObj(planetsData.results);
+
+  return planetObjArray;
+};
+
+
+const createPlanetObj = (planetArray) => {
+  const unresolvedPromises = planetArray.map( async (planet) => {
+    const fetchedResidents = await fetchResidents(planet.residents);
+    return {name: planet.name, data: {terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: fetchedResidents.join(',\n')}};
+  });
+  return Promise.all(unresolvedPromises);
+}
+
+const fetchResidents = (apiArray) => {
+  const unresolvedPromises = apiArray.map(async (api)=>{
+    const fetchResident = await fetch(api);
+    const residentData = await fetchResident.json();
+    return residentData.name;
+  }); 
+  
+  return Promise.all(unresolvedPromises);
+}
