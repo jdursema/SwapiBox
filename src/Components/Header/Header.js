8eb9f76ad5e8e0d8648ae4cb55feeef4331c063f
Julie Dursema <jmdursema@gmail.com>
2018-04-23 15:25:03 -0600

    remove un necessary div

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,13 +5,10 @@ import PropTypes from 'prop-types';
 const Header = ({ Favorites, displayFavorites }) => {
   return (
     <div className='header'>
-      
-      <div className='btn-container'>
-        <h1>SW(api) Box</h1>
+        <h1 className='title'>SW(api) Box</h1>
         <button onClick={()=>displayFavorites()} className='favorite-btn'>
           Favorites {Favorites.length}
         </button>
-      </div>
     </div>
   );
 };
